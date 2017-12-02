5cf967d60986a4cab59862dee0eb13e93735ae5b
Ray Lu <ray@codemeit.com>
2017-12-03 12:34:55 +1300

    Initial commit

---
+++ Blockchain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToyBitcoinBlockchain
+{
+    class Blockchain
+    {
+
+        List<Block> _chain;
+        public int Difficulty { get; set; }        
+
+        public Blockchain()
+        {
+            this._chain = new List<Block>();
+            this._chain.Add(CreateGenesisBlock());
+            this.Difficulty = 2;
+        }
+
+        Block CreateGenesisBlock()
+        {
+            return new Block(0, "01/12/2017", "Genesis block", "");
+        }
+
+        public Block GetLatestBlock()
+        {
+            return this._chain.Last();
+        }
+
+        public void AddBlock(Block newBlock)
+        {
+            newBlock.PreviousHash = this.GetLatestBlock().Hash;
+            newBlock.Mine(this.Difficulty);
+
+            this._chain.Add(newBlock);
+        }
+
+        public void ValidateChain()
+        {
+
+            for (var i = 1; i < this._chain.Count; i++)
+            {
+                var currentBlock = this._chain[i];
+                var previousBlock = this._chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    throw new Exception("Chain is not valid! Current hash is incorrect!");
+                }
+
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    throw new Exception("Chain is not valid! PreviousHash isn't pointing to the previous block's hash!");
+                }
+            }            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToyBitcoinBlockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCoinBlockchain = new Blockchain();
+            myCoinBlockchain.Difficulty = 3;
+
+            // Received a block from the P2P network.
+            // Validate 300 coins transfer.
+            Console.WriteLine("Mining a block...");
+            myCoinBlockchain.AddBlock(new Block(1, "03/12/2017", "300"));
+
+            // this line below will cause the chain to be invalid.
+            //myCoinBlockchain.GetLatestBlock().PreviousHash = "";
+
+            // Validate the chain
+            myCoinBlockchain.ValidateChain();
+            
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+
+
+}
